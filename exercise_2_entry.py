164cc7a971184ac60771423bb8ed828ccff49525
silverchair <carlosdanielh@mail.com>
2020-12-28 23:35:45 -0400

    exercise complited

--- exercise_2_entry.py
+++ exercise_2_entry.py
@@ -1,17 +1,22 @@
 from tkinter import *
+import os
 
 
-def sayhi():    
+def sayhi():
     print(f'hi {name.get()} {middle_name.get()} {last_name.get()}')
 
 
+os.system('cls')
 windows = Tk()
-windows.geometry('400x400')
+windows.geometry('400x400+400+150')
 windows.title('inputs Tkinter')
 name = StringVar()
 middle_name = StringVar()
 last_name = StringVar()
+age = IntVar()
+sex = StringVar()
 name.set('write your name')
+sex.set('M/F')
 
 
 label1 = Label(windows, text='write your name')
@@ -22,6 +27,12 @@ middle_name_box = Entry(windows, textvariable=middle_name)
 
 label3 = Label(windows, text='write your last name')
 last_name_box = Entry(windows, textvariable=last_name)
+
+label_age = Label(windows, text='Age: ')
+age_box = Entry(windows, textvariable=age)
+
+label_sex = Label(windows, text='Sex: ')
+sex_box = Entry(windows, textvariable=sex)
 
 button1 = Button(windows, text='custom hi', command=sayhi)
 
@@ -35,6 +46,12 @@ middle_name_box.place(x=170, y=40)
 label3.place(x=10, y=70)
 last_name_box.place(x=170, y=70)
 
-button1.place(x=10, y=100)
+label_age.place(x=10, y=100)
+age_box.place(x=45, y=100)
+
+label_sex.place(x=10, y=130)
+sex_box.place(x=45, y=130)
+
+button1.place(x=10, y=160)
 ############################################
 windows.mainloop()
